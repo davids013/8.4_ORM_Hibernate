b874bed4870ddb02f1444314c93c0e0de7fe0653
Vitaly Shvedko <davids013013@gmail.com>
2022-04-24 15:28:29 +0300

    Convert to JpaRepository

--- src/main/java/ru/netology/orm_hibernate/controller/PersonController.java
+++ src/main/java/ru/netology/orm_hibernate/controller/PersonController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.netology.orm_hibernate.db_entity.NameSurnameAge;
 import ru.netology.orm_hibernate.db_entity.Persons;
 import ru.netology.orm_hibernate.service.PersonService;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/persons")
@@ -21,7 +23,63 @@ public class PersonController {
     }
 
     @GetMapping("/by-city")
-    public List<Persons> getPersonsByCity(@RequestParam String city) {
-        return service.getPersonsByCity(city);
+    public List<Persons> getPersonsByCity(@RequestParam String cityOfLiving) {
+        return service.getPersonsByCity(cityOfLiving);
+    }
+
+    @GetMapping("/create")
+    public Persons create(@RequestParam String name,
+                          @RequestParam String surname,
+                          @RequestParam int age,
+                          @RequestParam String phoneNumber,
+                          @RequestParam String cityOfLiving) {
+        if (phoneNumber.isEmpty()) phoneNumber = null;
+        cityOfLiving = cityOfLiving.isEmpty() ? null : cityOfLiving.toUpperCase(Locale.ROOT);
+        final NameSurnameAge nsa = new NameSurnameAge(name, surname, age);
+        final Persons person = new Persons(nsa, phoneNumber, cityOfLiving);
+        return service.create(person);
+    }
+
+    @GetMapping("/read")
+    public Persons read(@RequestParam String name,
+                        @RequestParam String surname,
+                        @RequestParam int age) {
+        final NameSurnameAge id = new NameSurnameAge(name, surname, age);
+        final Persons p = service.read(id);
+        final Persons pp = new Persons(p.getNameSurnameAge(), p.getPhoneNumber(), p.getCityOfLiving());
+//        return p;
+        return pp;
+    }
+
+    @GetMapping("/update")
+    public Persons update(@RequestParam String name,
+                          @RequestParam String surname,
+                          @RequestParam int age,
+                          @RequestParam String phoneNumber,
+                          @RequestParam String cityOfLiving) {
+        if (phoneNumber.isEmpty()) phoneNumber = null;
+        cityOfLiving = cityOfLiving.isEmpty() ? null : cityOfLiving.toUpperCase(Locale.ROOT);
+        final NameSurnameAge nsa = new NameSurnameAge(name, surname, age);
+        final Persons person = new Persons(nsa, phoneNumber, cityOfLiving);
+        return service.update(person);
+    }
+
+    @GetMapping("/delete")
+    public boolean delete(@RequestParam String name,
+                          @RequestParam String surname,
+                          @RequestParam int age) {
+        final NameSurnameAge id = new NameSurnameAge(name, surname, age);
+        return service.delete(id);
+    }
+
+    @GetMapping("/by-age-younger")
+    public List<Persons> getByAgeYounger(@RequestParam int age) {
+        return service.getByAgeYounger(age);
+    }
+
+    @GetMapping("/by-name-surname")
+    public Persons getByNameAndSurname(@RequestParam String name,
+                                       @RequestParam String surname) {
+        return service.getByNameAndSurname(name, surname);
     }
 }
--- src/main/java/ru/netology/orm_hibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/orm_hibernate/repository/PersonRepository.java
@@ -1,53 +1,17 @@
 package ru.netology.orm_hibernate.repository;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.netology.orm_hibernate.db_entity.NameSurnameAge;
 import ru.netology.orm_hibernate.db_entity.Persons;
-import ru.netology.orm_hibernate.enums.City;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Random;
+import java.util.Optional;
 
-@Repository
-public class PersonRepository {
-    private boolean isDbEmpty = true;
+public interface PersonRepository extends JpaRepository<Persons, NameSurnameAge> {
+    List<Persons> findByCityOfLiving(String cityOfLiving, Sort sort);
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    List<Persons> findByNameSurnameAge_AgeLessThan(int age, Sort sort);
 
-    @Transactional
-    public List<Persons> getPersonsByCity(String city) {
-        if (isDbEmpty) {
-            if (checkDbSize() > 0) {
-                isDbEmpty = false;
-            } else fillDb();
-        }
-        final Query q = entityManager.createQuery(
-                String.format("select p from Persons p where p.cityOfLiving = '%s'", city.toUpperCase(Locale.ROOT)));
-        return (List<Persons>) q.getResultList();
-    }
-
-    private void fillDb() {
-        final List<Persons> persons = new ArrayList<>();
-        final Random rnd = new Random();
-        for (int i = 0; i < City.values().length; i++) {
-            final int j = i + 1;
-            final NameSurnameAge nsa =
-                    new NameSurnameAge("Name" + j, "Surname" + j, rnd.nextInt(81) + 18);
-            persons.add(new Persons(nsa, null, City.values()[i].toString()));
-        }
-        for (Persons p : persons)
-            entityManager.persist(p);
-    }
-
-    private int checkDbSize() {
-        final Query q = entityManager.createQuery("SELECT p FROM Persons p");
-        return (int) q.getResultStream().count();
-    }
+    Optional<Persons> findByNameSurnameAge_NameAndNameSurnameAge_Surname(String name, String surname);
 }
